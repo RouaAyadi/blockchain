50fd1aafeccd8f22c38067e704bdd3004a38f188
RouaAyadi <ayadiroua9@gmail.com>
2025-04-15 13:13:30 +0100

    add first function

---
+++ FundMe.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract FundMe{
+    function fund() public payable  {
+        require(msg.value >1e18,"there wasnt enought ETH");
+    }
+    function withdraw() public payable{}
+}
